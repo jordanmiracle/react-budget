0ed063a4d8d4e2dc4ac4b2423d7025af840b00f0
Jordan Miracle <jordanmiracle@protonmail.com>
2022-01-27 02:04:20 -0800

    App is working perfectly to this point. Two custom, individual components. ExpenseItem and ExpenseDate. Both are imported into app and working well.

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,7 @@
 function ExpenseDate(props) {
-
-    const month = props.date.toLocaleString('en-US', { month: 'long' })
-    const year = props.date.getFullYear()
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
 
     return (
         <div>
@@ -12,6 +10,7 @@ function ExpenseDate(props) {
             <div>{year}</div>
         </div>
     );
+
 }
 
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,15 +4,13 @@ import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">{}</div>
-            <div>
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
                 <h2>{props.title}</h2>
-
                 <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ExpenseItem;
