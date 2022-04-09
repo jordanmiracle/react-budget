8cec60a6d0764495f6778da08defe64da23539a5
Jordan Miracle <jordanmiracle@protonmail.com>
2022-04-09 01:58:52 -0700

    Added NewExpense and ExpenseForm.js with some basic styling. Added state to the forms. title, amount, and date change handlers.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Expenses from "./components/Expenses/Expenses";
 import React from 'react';
 
+import NewExpense from "./components/NewExpense/NewExpense";
+
 const App = () => {
   const expenses = [
     {
@@ -22,32 +24,32 @@ const App = () => {
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
+    id: 'e4',
       title: 'Game Pass Bill',
       amount: 16.59,
       date: new Date(2021, 5, 12),
     },
   ];
 
-  //  return (
-  //    <div>
-  //      <h2>Budget Calculator</h2>
-  //      <Expenses items={expenses} />
-  //    </div>
-  //  )
-  //
-  //}
-
-// This, below, is the same as above but with a different syntax //
-// This is the under the hood code that JSX "compiles" into. //
-
-
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h2', {}, 'Budget Calculator'),
-    React.createElement(Expenses, { items: expenses })
+  return (
+  <div>
+        <NewExpense />
+        <Expenses items={expenses} />
+      </div>
     );
-  };
+  
+  }
+  
+  // This, below, is the same as above but with a different syntax //
+  // This is the under the hood code that JSX "compiles" into. //
+
+
+//  return React.createElement(
+//    'div',
+//    {},
+//    React.createElement('h2', {}, 'Budget Calculator'),
+//    React.createElement(Expenses, { items: expenses })
+//    );
+ // };
 
   export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,20 +2,26 @@ import '../Expenses/ExpenseItem.css';
 import ExpenseDate from '../Expenses/ExpenseDate';
 import '../Expenses/ExpenseDate.css';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
+   
+const ExpenseItem = (props) => {
+    // function ExpenseItem(e) { // This is the function that will be called when the button is clicked. This is simply another syntax for the function started above.
+    const [title, setTitle] = useState(props.title); // This is the state that will be used to store the title of the expense.
 
-    const clickHandler = () => {
-        console.log('clicked!!');
+    const clickHandler = () => {  // This is the function that will be called when the button is clicked.
+        setTitle('Updated Title');
+        console.log(title);
     };
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={clickHandler}>Change Title</button>  {/* This is the syntax for calling the function that was created above. */}
         </Card>
     );
 }
